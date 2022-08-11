9495f1c9c278c13bd2a0d9d0599cb25900fefdb5
Jose Briones <josmbrio@gmail.com>
2022-08-11 00:04:18 -0500

    asdfdf

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,11 +1,11 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo "building the Image"
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t josmbrio/my-repo:jma-2.0 .'
+        sh "docker build -t $imageName ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push josmbrio/my-repo:jma-2.0"
+        sh "docker push $imageName"
     }
 
 }
--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
 def call() {
-    echo "building the Jar"
+    echo "building the Jar for branch $BRANCH_NAME"
     sh 'mvn package'
 }
